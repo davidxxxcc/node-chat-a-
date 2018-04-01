791083e06975a8099d3dd6c8d38a9cd2145ca48d
FrankThink <daivdxxxcc@gmail.com>
2018-04-01 13:39:08 +0800

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+const socket = io();
+
+socket.on('connect', function() {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'Frank',
+        text: 'Yup!'
+
+    });
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function(message) {
+    console.log('newMessage: ', message);
+})
